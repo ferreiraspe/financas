4454e4af498ddcd63aaedf3cbe44e49f83960f0c
Edson Ferreira <edson.da.silva@avanade.com>
2021-04-23 23:57:06 -0300

    start project finanças

---
+++ src/test/java/com/edson/financas/FinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edson.financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
