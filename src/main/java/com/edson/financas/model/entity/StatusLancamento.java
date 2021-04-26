31508f1f65da870c763e64c0293d71993dd7c3f4
ferreiraspe <ferreiraspe@gmail.com>
2021-04-26 00:19:32 -0300

    creating model/entity class

---
+++ src/main/java/com/edson/financas/model/entity/StatusLancamento.java
@@ -0,0 +1,8 @@
+package com.edson.financas.model.entity;
+
+public enum StatusLancamento {
+
+    PENDENTE,
+    CANCELADO,
+    EFETIVADO
+}
---
+++ src/main/java/com/edson/financas/model/entity/TipoLancamento.java
@@ -0,0 +1,7 @@
+package com.edson.financas.model.entity;
+
+public enum TipoLancamento {
+
+    RECEITA,
+    DESPESA
+}
