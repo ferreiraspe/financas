33fe22ff26cfc44da7840b94397e46aff0ff617a
ferreiraspe <ferreiraspe@gmail.com>
2021-05-06 23:02:40 -0300

    Implement method atualizarStatus in LancamentoResource class

--- src/main/java/com/edson/financas/api/resource/LancamentoResource.java
+++ src/main/java/com/edson/financas/api/resource/LancamentoResource.java
@@ -1,5 +1,6 @@
 package com.edson.financas.api.resource;
 
+import com.edson.financas.api.dto.AtualizaStatusDTO;
 import com.edson.financas.api.dto.LancamentoDTO;
 import com.edson.financas.exception.RegraNegocioException;
 import com.edson.financas.model.entity.Lancamento;
@@ -69,6 +70,26 @@ public class LancamentoResource {
             lancamento.setId(entity.getId());
             service.atualizar(lancamento);
             return ResponseEntity.ok(lancamento);
+            }catch (RegraNegocioException e){
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+        }).orElseGet(() ->
+                new ResponseEntity("Lançamento não encontrado na base de Dados.", HttpStatus.BAD_REQUEST));
+    }
+
+    @PutMapping("{id}/atualiza-status")
+    public ResponseEntity atualizarStatus(@PathVariable("id") Long id, @RequestBody AtualizaStatusDTO dto){
+        return service.obterPorId(id).map(entity -> {
+            StatusLancamento statusSelecionado = StatusLancamento.valueOf(dto.getStatus());
+
+            if (statusSelecionado == null){
+                return ResponseEntity.badRequest().body("Nao foi possível atualizar o status do lançamento, envie um status válido.");
+            }
+
+            try {
+                entity.setStatus(statusSelecionado);
+                service.atualizar(entity);
+                return ResponseEntity.ok(entity);
             }catch (RegraNegocioException e){
                 return ResponseEntity.badRequest().body(e.getMessage());
             }
